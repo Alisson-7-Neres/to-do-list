9f2034001c1af1390a213c139d5b105d484ba139
Alisson-7-Neres <alisson.neres@outlook.com.br>
2025-12-03 16:10:01 -0300

    Refatoração - Adicionar tarefas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 let calendar;
 document.addEventListener('DOMContentLoaded', function () {
-    const calendarEl = document.getElementById('calendar');
+     const calendarEl = document.getElementById('calendar');
 
     calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
@@ -16,55 +16,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const btnEvent = document.getElementById('btnEvent');
 const divTasks = document.getElementById('divTasks');
+const button = document.getElementById('btnAdd');
 let ol = null;
 
-btnEvent.addEventListener("click", function ()  {
+btnEvent.addEventListener("click", function () {
 
     btnEvent.style.display = 'none';
-
-    if(!ol) {
-    ol = document.createElement('ol');
-    divTasks.appendChild(ol);
-    }
-    // Criando <li><input type="text" placeholder="Título"></li> 
-    const li1 = document.createElement('li');
-    ol.appendChild(li1);
-
-    const inputTitle = document.createElement('input');
-    inputTitle.name = 'titulo';
-    inputTitle.id = 'titulo';
-    inputTitle.placeholder = 'Título';
-    li1.appendChild(inputTitle);
-
-    // Criando <li><input type="text" placeholder="Começo"></li> 
-    const li2 = document.createElement('li');
-    ol.appendChild(li2);
-    const inputStart = document.createElement('input');
-    inputStart.type = 'date';
-    inputStart.name = 'comeco';
-    inputStart.id = 'comeco';
-    inputStart.placeholder = 'Começo';
-    li2.appendChild(inputStart);
-
-    // Criando <li><input type="text" placeholder="Fim"></li>
-    const li3 = document.createElement('li');
-    ol.appendChild(li3);
-    const inputEnd = document.createElement('input');
-    inputEnd.type = 'date'
-    inputEnd.name = 'fim';
-    inputEnd.id = 'fim';
-    inputEnd.placeholder = 'Fim';
-    li3.appendChild(inputEnd);
-
-    // Criando button
-    const button = document.createElement('input');
-    button.type = 'button';
-    button.value = 'Adicionar';
-    button.id = 'btnAdd';
+    divTasks.style.display = 'block';
     button.addEventListener('click', adicionarTarefa);
-    divTasks.appendChild(button);
+
+    adicionarTarefa();
     
 });
+
 function adicionarTarefa() {
     const titleValue = document.getElementById('titulo').value;
     const startValue = document.getElementById('comeco').value;
@@ -83,8 +47,11 @@ function adicionarTarefa() {
         document.getElementById('comeco').value = '';
         document.getElementById('fim').value = '';
 
+        mostrarTarefas();
+
     } else {
         console.error('Calendário não está inicializando!');
     }
 
 }
+
